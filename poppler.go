489f3426eb38b1c88deb2505f40ac010acf39a2f
Johannes Bareuther <none@example.om>
2023-01-15 23:53:46 +0100

    dehyphenate and remove newlines

--- poppler.go
+++ poppler.go
@@ -45,11 +45,38 @@ func (d *Pdf) Text() (string) {
 
 	for n := 0; n < d.GetNPages(); n++ {
 		page := d.GetPage(n)
-		buf.WriteString(strings.TrimSpace(page.Text()))
+		buf.WriteString(dehyphenateString(page.Text()))
 		ch <- page
 	}
 	close(ch)
 	return buf.String()
+}
+
+// dehyphenateString replaces hyphens at the end of a line
+// with the first word from the following line, and removes
+// that word from its line.
+// taken from https://git.rescribe.xyz/cgit/cgit.cgi/utils/tree/cmd/dehyphenate/main.go
+func dehyphenateString(in string) string {
+	var newlines []string
+	lines := strings.Split(in, "\n")
+	for i, line := range lines {
+		words := strings.Split(line, " ")
+		last := words[len(words)-1]
+		// the - 2 here is to account for a trailing newline and counting from zero
+		if len(last) > 0 && last[len(last)-1] == '-' && i < len(lines)-2 {
+			nextwords := strings.Split(lines[i+1], " ")
+			if len(nextwords) > 0 {
+				line = line[0:len(line)-1] + nextwords[0]
+			}
+			if len(nextwords) > 1 {
+				lines[i+1] = strings.Join(nextwords[1:], " ")
+			} else {
+				lines[i+1] = ""
+			}
+		}
+		newlines = append(newlines, line)
+	}
+	return strings.Join(newlines, " ")
 }
 
 //StreamText writes the document's plain text content to an io.Writer
@@ -59,8 +86,9 @@ func (d *Pdf) StreamText(w io.Writer) {
 	log.Printf("Number of Pages: %d", d.GetNPages())
 	for n := 0; n < d.GetNPages(); n++ {
 		page := d.GetPage(n)
-		pText := strings.TrimSpace(page.Text())
-		w.Write([]byte(pText))
+		dehyph := dehyphenateString(page.Text())
+		// pText := strings.TrimSpace(page.Text())
+		w.Write([]byte(dehyph))
 		ch <- page
 	}
 	close(ch)
