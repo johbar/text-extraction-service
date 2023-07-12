37077994c42096811e78df59d18824840a7f9ef5
Johannes Bareuther <none@example.om>
2023-07-12 23:19:57 +0200

    add config
    - enable use of external Nats server
    - don't expose embedded Nats by default

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"runtime/debug"
 	"time"
@@ -24,7 +25,19 @@ var (
 	metadataBucket       nats.KeyValue
 )
 
+const (
+	// config items
+	confHostPort   = "host_port"
+	confMaxPayload = "max_payload"
+	confExposeNats = "expose_nats"
+	confNatsPort   = "nats_port"
+	confNatsHost   = "nats_host"
+	confExtNats    = "external_nats"
+	confNatsDir    = "nats_store_dir"
+)
+
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
 	if os.Getenv("GOMEMLIMIT") != "" {
 		log.Printf("GOMEMLIMIT=%v (%d MiB)", debug.SetMemoryLimit(-1), debug.SetMemoryLimit(-1)/1024/1024)
 	}
@@ -43,33 +56,66 @@ func main() {
 	// wg := startWorkers()
 
 	viper.SetEnvPrefix("tes")
-	viper.SetDefault("host", ":8080")
-	viper.SetDefault("max_payload", 8*1024*1024)
+	// This service
+	viper.SetDefault(confHostPort, ":8080")
+	viper.SetDefault(confMaxPayload, 8*1024*1024)
+	viper.SetDefault(confExposeNats, false)
+	viper.SetDefault(confNatsPort, 4222)
+	viper.SetDefault(confNatsHost, "localhost")
+	viper.SetDefault(confExtNats, false)
+
 	viper.AutomaticEnv()
-	srv.Addr = viper.GetString("host")
 
+	srv.Addr = viper.GetString(confHostPort)
 	srv.Handler = router
-	var maxPayload int32 = viper.GetInt32("max_payload")
+	maxPayload := viper.GetInt32(confMaxPayload)
+	natsHost := viper.GetString(confNatsHost)
+	natsPort := viper.GetInt(confNatsPort)
 
-	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
-	ns, err := server.NewServer(
-		&server.Options{
-			// Host:               "localhost",
-			JetStream:          true,
-			MaxPayload:         maxPayload,
-			JetStreamMaxMemory: 1024 * 1000,
-			TLS:                false,
-		})
-	if err != nil {
-		panic(err)
-	}
-	ns.ConfigureLogger()
-	ns.Start()
-	if !ns.ReadyForConnections(1 * time.Second) {
-		panic("Nats not ready!")
-	}
+	useExtNats := viper.GetBool(confExtNats)
+	var err error
+	if !useExtNats {
+		ns, err := server.NewServer(
+			&server.Options{
+				// Host:               "localhost",
+				JetStream:          true,
+				MaxPayload:         maxPayload,
+				JetStreamMaxMemory: 1024 * 1000,
+				TLS:                false,
+				DontListen:         !viper.GetBool("expose_nats"),
+				Host:               natsHost,
+				Port:               natsPort,
+				StoreDir: viper.GetString(confNatsDir),
+			})
+		if err != nil {
+			panic(err)
+		}
+		ns.ConfigureLogger()
+		ns.Start()
+		if !ns.ReadyForConnections(3 * time.Second) {
+			panic("Nats not ready!")
+		}
 
-	nc, _ = nats.Connect(ns.ClientURL())
+		if err != nil {
+			panic(err)
+		}
+		nc, err = nats.Connect(ns.ClientURL(),
+			// connect in-process rather then per TCP
+			func(o *nats.Options) error {
+				o.InProcessServer = ns
+				return nil
+			})
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		connStr := fmt.Sprintf("nats://%s:%d", natsHost, natsPort)
+		log.Printf("Connecting to Nats %s", connStr)
+		nc, err = nats.Connect(connStr)
+		if err != nil {
+			panic(err)
+		}
+	}
 	js, err := nc.JetStream()
 	if err != nil {
 		log.Fatalf("%e", err)
@@ -93,7 +139,6 @@ func main() {
 		log.Printf("ERROR in webserver: %v", err)
 	}
 	log.Printf("Nats server still running...")
-	ns.WaitForShutdown()
 	log.Println("HTTP Server stopped.")
 
 }
