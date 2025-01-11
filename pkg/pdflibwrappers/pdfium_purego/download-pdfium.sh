1a3afda0825e964931e4f6a9ae36dea2c88df011
Johannes Bareuther <40851416+johbar@users.noreply.github.com>
2025-01-11 22:20:14 +0100

    Feature/release pipeline (#9)
    * feat: make download script honor GOARCH and GOOS
    
    * refactor pipelines and PDFium downloader
    
    - strip libs
    - remove UPX
    - gzip artifacts
    - recrate draft release on git tag
    - add more useful devcontainer definition
    
    * pipeline: fix handling of Musl/arch, strip on MacOS
    
    * pipeline: fix order of musl and arch; make strip work on Linux & MacOS
    
    * pipeline: fix musl/arch/os handling, strip more  symbols on both OSes
    
    * CI: add file tool to alpine; make the best of strip in both, Linux and MacOS
    
    * CI: install binutils on Alpine, make strip more effective on Linux
    
    * ci: build arm64 and amd64 artifacts on ubuntu, alpine and darwin
    
    * refactor stripping
    
    * make strip work on alpine; merge release artifacts
    
    * fix names of linux binaries
    
    * release-CI: add checksum
    
    * release-CI: rename checksum file
    
    * release CI: include sha256sum.txt in release artifacts

--- pkg/pdflibwrappers/pdfium_purego/download-pdfium.sh
+++ pkg/pdflibwrappers/pdfium_purego/download-pdfium.sh
@@ -1,45 +1,80 @@
 #!/usr/bin/env sh
 
 ## 
-# Loads the latest pdfium shared lib from Github for the current platform
+# Loads the latest pdfium shared lib from Github.
+# In honors GOOS and GOARCH but only for combinations of linux and darwin with arm64 and am64
 # This scripts aims to be platform independent and should work on MacOS and Linux (glibc or musl).
-
 ##
+
 set -o nounset
 set -o errexit
 # set -x
 
-os=$(uname)
-arch=$(uname -m)
-musl="$(ldd '/bin/true' | grep -qF musl && printf '-musl' || true)"
+goos=$(go env GOOS)
+arch=$(go env GOARCH)
+linux_arch="${arch}"
 
 my_path=$(readlink -f $0)
-my_dir=$(dirname "$my_path")
+my_dir=$(dirname "${my_path}")
 ext=''
 
-if test "$arch" = 'x86_64' ; then
+# translate GOARCH to PDFium and Linux architecture IDs
+# and abort if not amd64 or arm64
+case "${arch}" in
+  'amd64')
     arch='x64'
-fi
-
-case $os in
-  'Linux')
-    os="linux${musl}"
-    ext='so'
+    linux_arch='x86_64'
     ;;
-  'Darwin')
-    os='mac'
-    ext='dylib'
+  'arm64')
+    linux_arch='aarch64'
+    ;;
+  *)
+    printf "not a supported architecture: %s\n" "${arch}"
+    exit 1;
     ;;
 esac
 
-printf "arch=%s\n" "$arch"
-printf "os=%s\n" "$os"
-printf "my_dir=%s\n" "$my_dir\n"
+# translate GOOS to OS identifiers used on PDFium download page
+# also set the dynamic lib file extension and find out if it is a Musl distro
+case ${goos} in
+  'linux')
+    ext='so'
+    musl="$(ldd '/bin/true' | grep -qF musl && printf '-musl' || true)"
+    os="linux${musl}"
+    ;;
+  'darwin')
+    os='mac'
+    ext='dylib'
+    ;;
+  *)
+    printf "not a supported OS: %s\n" "${os}"
+    exit 1;
+    ;;
+esac
+
+try_to_strip () {
+    du -h "${lib_path}"
+    printf "Trying to strip...\n"
+    if test "${os}" = 'mac' && strip -S -x "${1}"; then
+      du -h "${1}"
+    fi
+    if test "${goos}" = 'linux'; then 
+      for strip_bin in strip /usr/bin/*strip; do
+        "$strip_bin" "${1}" && break || continue
+      done
+      du -h "${1}"
+    fi
+}
 
 url="https://github.com/bblanchon/pdfium-binaries/releases/latest/download/pdfium-${os}-${arch}.tgz"
 
-printf "Downloading %s to %s\n" "$url" "$my_dir"
+printf "Downloading %s\n" "${url}"
+lib_path="lib/libpdfium.${ext}"
 (
-    cd "$my_dir"
-    wget  -q -O - "$url" | tar -xzv lib/libpdfium.${ext}
+    cd "${my_dir}"
+    wget  -q -O - "${url}" | tar -xzv "${lib_path}"
+    printf "Extracted lib to %s/lib/libpdfium.%s\n" "${my_dir}" "${ext}"
+    file "${lib_path}" || true
+    try_to_strip "${lib_path}"
+    printf "Done.\n"
 )
