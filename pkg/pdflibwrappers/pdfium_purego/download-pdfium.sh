134c8783b469ae2523026da5ad400cbaa7ff9429
Johannes Bareuther <40851416+johbar@users.noreply.github.com>
2025-01-11 21:09:20 +0100

    refactor stripping

--- pkg/pdflibwrappers/pdfium_purego/download-pdfium.sh
+++ pkg/pdflibwrappers/pdfium_purego/download-pdfium.sh
@@ -11,21 +11,22 @@ set -o errexit
 # set -x
 
 goos=$(go env GOOS)
-arch="$(go env GOARCH)"
+arch=$(go env GOARCH)
+linux_arch="${arch}"
 
 my_path=$(readlink -f $0)
 my_dir=$(dirname "${my_path}")
 ext=''
 
-
+# translate GOARCH to PDFium and Linux architecture IDs
+# and abort if not amd64 or arm64
 case "${arch}" in
   'amd64')
     arch='x64'
-    ;;
-  '386')
-    arch='x86'
+    linux_arch='x86_64'
     ;;
   'arm64')
+    linux_arch='aarch64'
     ;;
   *)
     printf "not a supported architecture: %s\n" "${arch}"
@@ -33,6 +34,8 @@ case "${arch}" in
     ;;
 esac
 
+# translate GOOS to OS identifiers used on PDFium download page
+# also set the dynamic lib file extension and find out if it is a Musl distro
 case ${goos} in
   'linux')
     ext='so'
@@ -49,6 +52,17 @@ case ${goos} in
     ;;
 esac
 
+try_to_strip () {
+    du -h "${lib_path}"
+    printf "Trying to strip...\n"
+    if test "${os}" = 'mac' && strip -S -x "${1}"; then
+      du -h "${1}"
+    fi
+    if test "${goos}" = 'linux' && /usr/bin/${linux_arch}-linux-gnu-strip "${1}"; then 
+      du -h "${1}"
+    fi
+}
+
 url="https://github.com/bblanchon/pdfium-binaries/releases/latest/download/pdfium-${os}-${arch}.tgz"
 
 printf "Downloading %s\n" "${url}"
@@ -58,13 +72,6 @@ lib_path="lib/libpdfium.${ext}"
     wget  -q -O - "${url}" | tar -xzv "${lib_path}"
     printf "Extracted lib to %s/lib/libpdfium.%s\n" "${my_dir}" "${ext}"
     file "${lib_path}" || true
-    du -h "${lib_path}"
-    printf "Trying to strip...\n"
-    if test "${os}" = 'mac' && strip -S -x "${lib_path}"; then
-      du -h "${lib_path}"
-    fi
-    if test "${goos}" = "linux" && strip -s "${lib_path}"; then 
-      du -h "${lib_path}"
-    fi
+    try_to_strip "${lib_path}"
     printf "Done.\n"
 )
