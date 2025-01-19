b5f01252332b6fdd4750e4f9e2125b5580dc31e7
Johannes Bareuther <40851416+johbar@users.noreply.github.com>
2025-01-20 00:12:08 +0100

    check in missing file

--- internal/pdfproc/pdf_images.go
+++ internal/pdfproc/pdf_images.go
@@ -2,11 +2,14 @@
 package pdfproc
 
 import (
+	"bytes"
 	"io"
 	"strconv"
 	"time"
 
+	"github.com/pdfcpu/pdfcpu/pkg/api"
 	pdfcpuapi "github.com/pdfcpu/pdfcpu/pkg/api"
+	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu"
 	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu/model"
 	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu/types"
 )
@@ -21,10 +24,34 @@ var pdfConf *model.Configuration
 
 func init() {
 	pdfConf = model.NewDefaultConfiguration()
-	pdfConf.Optimize = false
-	pdfConf.OptimizeResourceDicts = false
 	pdfConf.ValidateLinks = false
 	pdfConf.Offline = true
+	pdfConf.Cmd = model.EXTRACTIMAGES
+}
+
+// Parses a PDF file in-memory for extracting images
+func ParseForImageExtraction(pdfData []byte) (*model.Context, error) {
+	var ctx *model.Context
+	rs := bytes.NewReader(pdfData)
+	ctx, err := api.ReadValidateAndOptimize(rs, pdfConf)
+	if err != nil {
+		return nil, err
+	}
+	return ctx, nil
+}
+
+func GetImages(ctx *model.Context, page int) ([]model.Image, error) {
+	images, err := pdfcpu.ExtractPageImages(ctx, page, false)
+	println("len: ", len(images))
+	if err != nil {
+		return nil, err
+	}
+
+	var imgSlice ([]model.Image)
+	for _, img := range images {
+		imgSlice = append(imgSlice, img)
+	}
+	return imgSlice, nil
 }
 
 // ProcessImages applies readFunc to every image found on the page with the specified zero-based page number
