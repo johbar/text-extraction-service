f511c1ce86a71a2604ba6d3109766e20abeec369
Johannes Bareuther <40851416+johbar@users.noreply.github.com>
2025-02-09 21:15:11 +0100

    feat(tesswrap): add unit test

--- pkg/tesswrap/tesswrap.go
+++ pkg/tesswrap/tesswrap.go
@@ -3,7 +3,6 @@ Package tesswrap is a rather limited wrapper for Tesseract OCR v5.
 It defaults to using the CLI.
 Alternative interfaces/implementations can be used by supplying build tags.
 
-This is work in progress.
 */
 package tesswrap
 
---
+++ pkg/tesswrap/tesswrap_test.go
@@ -0,0 +1,26 @@
+package tesswrap
+
+import (
+	"os"
+	"testing"
+)
+
+func TestImageToText(t *testing.T) {
+	if !Initialized {
+		t.Log("Tesseract not available")
+		return
+	}
+	f, err := os.Open("testdata/readme.png")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+	txt, err := ImageReaderToText(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(txt) == 0 {
+		t.Fatal("zero-length content")
+	}
+	t.Log(txt)
+}
