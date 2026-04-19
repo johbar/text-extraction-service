38acc198c210def8ec0e9933b182d36f68b2211c
Johannes Bareuther <40851416+johbar@users.noreply.github.com>
2026-04-19 14:15:37 +0200

    chore(officexmlparser): fix typo in docs

--- pkg/officexmlparser/doc.go
+++ pkg/officexmlparser/doc.go
@@ -1,3 +1,3 @@
-// Package officexmlparser implements functions to extract text an metadata from XML-based office documents.
+// Package officexmlparser implements functions to extract text and metadata from XML-based office documents.
 // At the moment these are odt, odp, docx, pptx.
 package officexmlparser
